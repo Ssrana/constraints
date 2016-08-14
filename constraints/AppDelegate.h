02ca3458fbe1c446618a9a27dbd832f74e9b5203
Sanchika <rana.sanchika@gmail.com>
2016-08-15 01:21:08 +0530

    Auto layouts with constraints

---
+++ constraints/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  constraints
+//
+//  Created by Sanchika on 06/03/16.
+//  Copyright © 2016 Sanchika. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ constraints/MainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewController.swift
+//  constraints
+//
+//  Created by Sanchika on 10/08/16.
+//  Copyright © 2016 Sanchika. All rights reserved.
+//
+
+import Foundation
